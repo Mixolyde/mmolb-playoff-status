3a468a7f519c0cd4714afafd28a62e420f04b616
Brian Grey <mixolyde@gmail.com>
2025-07-31 00:56:33 +0200

    Comment out debug prints

--- lib/stats/sim_postseason.dart
+++ lib/stats/sim_postseason.dart
@@ -2,9 +2,9 @@ import 'package:mmolb_playoff_status/stats/sim_utils.dart';
 
 //simulate complete playoff run
 void simulateUnstartedPostSeason(List<List<TeamSim>> simsByLeague){
-  print("Simulating unstarted postseason for ${simsByLeague.length} leagues");
+  //print("Simulating unstarted postseason for ${simsByLeague.length} leagues");
   int teamCount = simsByLeague.expand((simList) => simList).length;
-  print("Simulating unstarted postseason for $teamCount teams");
+  //print("Simulating unstarted postseason for $teamCount teams");
 
   // figure out the wild cards from both leagues
   simsByLeague[0].sort();
@@ -17,7 +17,7 @@ void simulateUnstartedPostSeason(List<List<TeamSim>> simsByLeague){
   wildCardRoundSims.add(simsByLeague[1][0]);
   wildCardRoundSims.add(simsByLeague[1][1]);
 
-  print("WildCardRoundSims from top 2 of each league: ${wildCardRoundSims.join('\n')}");
+  //print("WildCardRoundSims from top 2 of each league: ${wildCardRoundSims.join('\n')}");
 
   //join all leagues into one league to find wild card teams
   var allWildSims = <TeamSim>[];
@@ -25,7 +25,7 @@ void simulateUnstartedPostSeason(List<List<TeamSim>> simsByLeague){
   allWildSims.removeWhere((sim) => wildCardRoundSims.contains(sim));
   allWildSims.sort();
 
-  print("Selecting wild card teams from: ${allWildSims.length} teams");
+  //print("Selecting wild card teams from: ${allWildSims.length} teams");
   wildCardRoundSims.addAll(allWildSims.take(2));
 
   for(var sim in wildCardRoundSims) {
@@ -51,7 +51,7 @@ void simulateUnstartedPostSeason(List<List<TeamSim>> simsByLeague){
   for(var sim in slRoundSims) {
     sim.slSeries = true;
   }
-  print('slRoundSims: ${slRoundSims.join('\n')}');
+  //print('slRoundSims: ${slRoundSims.join('\n')}');
   
   //simulate 3 win series with 1st place league team and wild card round winner
   var slWinner1 = simulateSeries(slRoundSims[0], slRoundSims[2], 3, teamCount);
@@ -60,15 +60,15 @@ void simulateUnstartedPostSeason(List<List<TeamSim>> simsByLeague){
   var leagueChampSims = <TeamSim>[];
   leagueChampSims.add(slWinner1);
   leagueChampSims.add(slWinner2);
-  print('leagueChampSims: ${leagueChampSims.join('\n')}');
   for (var sim in leagueChampSims) {
     sim.mmolbSeries = true;
   }
+  //print('leagueChampSims: ${leagueChampSims.join('\n')}');
 
   // mmolb champ round
   //simulate 4 win series with subleague winners
   var mmolbWinner = simulateSeries(leagueChampSims[0], leagueChampSims[1], 4, teamCount);
   mmolbWinner.mmolbChamp = true;
-  print('mmolbWinner: $mmolbWinner');
+  //print('mmolbWinner: $mmolbWinner');
   
 }
